c5fcaa2deaab5f7ab1b9f1c38338839eac956365
Rossignol <ha.rossignol@laposte.net>
2021-11-13 19:00:07 +0100

    added timer

---
+++ src/ProgressBar.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let progress = 0
+</script>
+
+<div bp="grid">
+  <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+    <div class="progress-bar" style="width:{progress}%;" />
+  </div>
+</div>
+
+<style>
+  .progress-container {
+    margin: 0.2rem 0rem;
+    border: solid #727070 0.1rem;
+    border-radius: 0.5rem;
+  }
+  .progress-bar {
+    height: 1.5rem;
+    background-color: rgb(32, 245, 4);
+    border-top-left-radius: 0.5rem;
+    border-bottom-left-radius: 0.5rem;
+  }
+</style>
---
+++ src/Timer.svelte
@@ -0,0 +1,30 @@
+<script>
+ import ProgressBar from './ProgressBar.svelte'
+</script>
+
+<!------------------------------------------ HTML section ----------------->
+
+<div bp="grid">
+ <h2 bp="offset-5@md 4@md 12@sm">Seconds left :</h2>
+</div>
+
+<ProgressBar/>
+
+<button class="start">
+ start
+</button>
+<!------------------------------------------ CSS section ----------------->
+
+<style>
+
+ h2 {
+  margin: 0;
+ }
+
+ .start {
+  background-color: rgba(41, 132, 175, 0.986);
+  border-radius: .5rem;
+  width: 100%;
+  margin: .3rem;
+ }
+</style>
