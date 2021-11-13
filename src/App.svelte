abbc63afd39622a92f0495ca99a2002bf82510b8
Rossignol <ha.rossignol@laposte.net>
2021-11-13 17:34:25 +0100

    added html setup

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+ import Timer from "./Timer.svelte" 
+ import Howto from "./HowTo.svelte" 
+</script>
+
+
+<!------------------------------------------ HTML section ----------------->
+<h1>Handwashing App</h1>
+
+<Timer/>
+<Howto/>
+
+<h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+    Picture Source
+  </a>
+
+  <a href="https://freesound.org/people/metrostock99/sounds/345086/ ">
+    Sound Source
+  </a>
+</h3>
+<!------------------------------------------ CSS section ----------------->
+<style>
+h1, h3 {
+ text-align: center;
+}
+
+</style>
---
+++ src/ProgressBar.svelte
@@ -0,0 +1,26 @@
+<div bp="grid">
+   <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+
+     <div bp="offset-5@md 4@md 12@sm" style="width:20%" class="progress-bar" >
+     <span class="sr-only">%</span>
+     </div>
+  </div>
+</div>
+
+<style>
+ .progress-container {
+  margin: .2rem 0rem;
+  border: solid #727070 .1rem;
+  border-radius: .5rem;
+ }
+ .progress-bar {
+  height: 1.5rem;
+  background-color: rgb(32, 245, 4);
+  border-top-left-radius: .5rem;
+  border-bottom-left-radius: .5rem;
+ }
+ 
+ 
+
+
+</style>
